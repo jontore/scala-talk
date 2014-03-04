c33bcb67e0df84ef0d29fe559cb2a3e4f848a974
Jon Tore Hafstad <jontore@gmail.com>
2014-03-04 22:14:55 +0100

    Added a couple of list examples

---
+++ examples/list.scala
@@ -0,0 +1,33 @@
+object ListDemo {
+
+  def print[T](xs: List[T]): String = {
+    xs match {
+      case List() => ""
+      case x :: xs1 => x + "---" + print(xs1)
+    }
+  }
+
+  def higherThanThreshold(t: Int, xs: List[Int]): Int = {
+    val found = xs find (_ > t)
+    found match {
+      case Some(f) => f
+      case None => 0
+    }
+  }
+
+  def multiplyList(xs: List[Int]): Int = {
+    (1 /: xs) (_ * _)
+  }
+
+}
+
+
+
+//Should print formatted list
+println(ListDemo.print(List.range(1, 5)))
+
+// Should print 10
+println(ListDemo.higherThanThreshold(5, List(2, 1, 2, 4, 10, 5, 7, 2)))
+
+//Should
+println(ListDemo.multiplyList(List.range(1, 5)))
